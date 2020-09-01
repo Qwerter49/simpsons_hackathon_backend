c19f2d9d87897b37832b3f3954c39471abd2c0c5
Sam Evans <evansst@icloud.com>
2020-09-01 14:22:34 -0600

    fixes a bug in the character model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 QuoteJoin.destroy_all
 Character.destroy_all
 Quote.destroy_all
-#counter = 1
-10.times do
+
+15.times do
     response = RestClient.get("https://thesimpsonsquoteapi.glitch.me/quotes?count=10")
 
-    data=JSON.parse(response)
+    data = JSON.parse(response)
     data.map do |result|
 
         new_character = Character.find_or_create_by(name: result["character"]) do |new_character|
@@ -15,7 +15,6 @@ Quote.destroy_all
         new_quote = Quote.find_or_create_by(quote: result["quote"])
 
         if !QuoteJoin.find_by(quote_id: new_quote.id)
-
             QuoteJoin.create(character_id: new_character.id, quote_id: new_quote.id)
         end
     end
